482806e26b31903ab10f0396addfae52ce315f9a
Carlos Sol <fersolmarin@hotmail.com>
2019-06-24 17:03:33 -0500

    displayGeneral.vue Created
    Branch Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <displayGeneral />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import displayGeneral from './components/displayGeneral.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    displayGeneral
   }
 }
 </script>
