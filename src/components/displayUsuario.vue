bae55911cb8b2a40c60dc9e045016c90aa13b1a2
Carlos Sol <fersolmarin@hotmail.com>
2019-06-29 22:45:19 -0500

    Added Tejuino FW & SCSS support

--- src/components/displayUsuario.vue
+++ src/components/displayUsuario.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="page">
-
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          public\assets\css\tejuino\tejuino.css
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
