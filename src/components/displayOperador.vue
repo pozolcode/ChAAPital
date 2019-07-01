47acfe01c08b357573bcfc92f44849240df45653
Carlos Sol <fersolmarin@hotmail.com>
2019-07-01 15:22:38 -0500

    Component Display & DB Logic Finished

--- src/components/displayOperador.vue
+++ src/components/displayOperador.vue
@@ -22,7 +22,33 @@
     </div>
     <!-- Operator Display Menu -->
     <div class="container operator-menu" v-else-if="logged">
-
+      <!-- Welcome Row -->
+      <div class="row">
+        <div class="col-5 welcome">Bienvenido Operador</div>
+      </div>
+      <!-- Current Turn Row -->
+      <div class="row">
+        <div class="col-5">Atendiendo Turno {{ dataTurn }}</div>
+      </div>
+      <!-- Company Data Row -->
+      <div class="row">
+        <div class="col-5">Armadora: {{ dataCompany }}</div>
+      </div>
+      <!-- Model Data Row -->
+      <div class="row">
+        <div class="col-5">Modelo: {{ dataModel }}</div>
+      </div>
+      <!-- Year Data Row -->
+      <div class="row">
+        <div class="col-5">Año: {{ dataYear }}</div>
+      </div>
+      <!-- Motor Data Row -->
+      <div class="row">
+        <div class="col-5">Motor: {{ dataMotor }}</div>
+        <div class="col-7">
+          <button @click="fetchTurn">Siguiente</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +60,12 @@ export default {
     return {
       logged: false,
       user: '',
-      password: ''
+      password: '',
+      dataTurn: '',
+      dataCompany: '',
+      dataModel: '',
+      dataYear: '',
+      dataMotor: ''
     }
   },
   methods: {
@@ -50,16 +81,48 @@ export default {
       // Dummy Variables
       if (this.user === dummyUser && this.password === dummyPassword) {
         this.logged = true;
+        this.getData();
       } else {
         alert("Usuario y/o Contraseña Incorrectos\nInténtelo de nuevo");
       }
       this.user = "";
       this.password = "";
+    },
+    getData: function () {
+      // Apollo Handling
+      this.dataTurn = "1";
+      this.dataCompany = "Placeholder";
+      this.dataModel = "Placeholder";
+      this.dataYear = "Placeholder";
+      this.dataMotor = "Placeholder";
+      // Apollo Handling
+    },
+    pushTurn: function () {
+      // Apollo Handling
+      // GETs next turn data
+      // Apollo Handling
+    },
+    fetchTurn: function () {
+      this.pushTurn();
+      this.getData();
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+button {
+  background-color: #f18d2a;
+  color: #ffffff;
+  font-size: 4rem;
+  height: 10rem;
+  width: 40rem;
+  &:active {
+    background-color: #b3661a;
+  }
+}
 
+.row { justify-content: flex-start; }
+
+.welcome { text-transform: uppercase; }
 </style>
