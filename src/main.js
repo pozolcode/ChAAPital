a1a186c2516699e7ec61e8caeb25edc25690e819
Carlos Sol <fersolmarin@hotmail.com>
2019-07-01 21:19:17 -0500

    Apollo Integration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 Vue.config.productionTip = false
 
-new Vue({
+Vue.use(VueApollo)
+
+// HTTP connection to the API
+const httpLink = createHttpLink ({
+  // You should use an absolute URL here
+  uri: 'https://chapital-counter.herokuapp.com/v1/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache ()
+
+// Create the apollo client
+const apolloClient = new ApolloClient ({
+  link: httpLink,
+  cache,
+})
+
+const apolloProvider = new VueApollo ({
+  defaultClient: apolloClient,
+})
+
+new Vue ({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
