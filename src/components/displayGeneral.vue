fd41cbaabfb8928b4cc15d96f9fb4a8d2ee5dd8c
Miguel May <miguelcastilla50@gmail.com>
2019-07-04 15:19:58 -0500

    Partial GraphQL Calls

--- src/components/displayGeneral.vue
+++ src/components/displayGeneral.vue
@@ -2,10 +2,10 @@
   <div class="page">
     <div class="container">
       <!-- Current Turn Row -->
-      <div class="row">
-        <div class="col-12"><span class="window-text">Turno</span></div>
-        <div class="col-12"><span class="window-number">{{ currentTurn }}</span></div>
-      </div>
+        <div class="row">
+          <div class="col-12"><span class="window-text">Turno</span></div>
+          <div class="col-12"><span class="window-number">{{ currentTurn }}</span></div>
+        </div>
       <!-- Open Operator Row -->
       <div class="row">
         <div class="col-12"><span class="window-text">Ventana</span></div>
@@ -16,15 +16,41 @@
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
   name: 'displayGeneral',
   data: function () {
     return {
-      currentTurn: 'Placeholder',
-      openOperator: 'Placeholder'
+      currentTurn: 0,
+      openOperator: 0
+    }
+  },
+  // Apollo Calls to fill variable names
+  apollo: {
+    currentTurn: {
+      query: gql`
+      query getTurns {
+        turns (limit: 1, where: {status: {_eq: "free"}, speedCheck: {_eq: false}}) {
+          id
+        }
+      }`,
+      update (data) {
+        return data.turns.currentTurn;
+      }
+    },
+    openOperator: {
+      query: gql`
+      query getOperator {
+        operatorList (limit: 1, where: {active: {_eq: true}, type: {_eq: 1}}) {
+          id
+        }
+      }`,
+      update (data) {
+        return data.operatorList.openOperator;
+      }
     }
   }
-  // Apollo Calls to fill variable names
 }
 </script>
 
